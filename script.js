089153b3c9176813429fa3d7c66eef34444b8a03
Tejas Kumar S <tej0897@gmail.com>
2024-05-07 08:45:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
-const freeRegex = /free money/i;
+const freeRegex = /fr[e3][e3] mon[e3]y/i;
 
 const denyList = [helpRegex, dollarRegex, freeRegex];
 
